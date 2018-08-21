e2793377890f05196ca73dd4a021aa0d03aebf0a
Selena <selena@hawaii-macbook-air.local>
2018-08-21 20:06:14 +0800

    Initial Commit

---
+++ AppDevWeek6_2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppDevWeek6_2
+//
+//  Created by Selena on 2018/8/21.
+//  Copyright © 2018年 Selena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

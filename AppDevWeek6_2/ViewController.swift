d64b2f9d3425d7a42ed0400873bbea09e9995ab5
Selena <selena@hawaii-macbook-air.local>
2018-08-21 22:09:48 +0800

    finish

--- AppDevWeek6_2/ViewController.swift
+++ AppDevWeek6_2/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ageSliderControl: UISlider!
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var zodiacTextField: UITextField!
+    @IBOutlet weak var genderSegmented: UISegmentedControl!
+    @IBOutlet weak var gameplayerSegmented: UISegmentedControl!
+    @IBOutlet weak var petSegmented: UISegmentedControl!
+    @IBOutlet weak var doveImage: UIImageView!
+    @IBOutlet weak var answerImage: UIImageView!
+    @IBOutlet weak var answerLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func selectTrueLove(_ sender: Any) {
+        if zodiacTextField.text == "雙魚"
+            && ageLabel.text == "32"
+            && genderSegmented.selectedSegmentIndex == 1
+            && petSegmented.selectedSegmentIndex == 0
+            && gameplayerSegmented.selectedSegmentIndex == 0
+                {
+            answerImage.image = UIImage(named: "dove-2")
+            answerLabel.text = "YES YOU ARE!"
+        } else {
+            answerImage.image = UIImage(named: "dove-3")
+            answerLabel.text = "NO HELL NO!"
+        }
+    }
+    
+    @IBAction func ageSlider(_ sender: Any) {
+        ageLabel.isHidden = false
+        ageLabel.text = String(Int(ageSliderControl.value))
+    }
+    
 }
 

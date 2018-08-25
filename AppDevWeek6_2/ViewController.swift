51e00c4f926593c0dee430e1f6c99d63e0b04478
Selena <selena@2001-b011-400a-1062-0c17-78c5-037f-cf9c.dynamic-ip6.hinet.net>
2018-08-26 00:30:15 +0800

    finished

--- AppDevWeek6_2/ViewController.swift
+++ AppDevWeek6_2/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //宣告storyboard裡所有元件的型別
     @IBOutlet weak var ageSliderControl: UISlider!
     @IBOutlet weak var ageLabel: UILabel!
     @IBOutlet weak var zodiacTextField: UITextField!
@@ -19,30 +20,46 @@ class ViewController: UIViewController {
     @IBOutlet weak var doveImage: UIImageView!
     @IBOutlet weak var answerImage: UIImageView!
     @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var numberStepper: UIStepper!
+    @IBOutlet weak var stepperValue: UILabel!
+    var numValue:Int=0 //設置stepperValue的值為Int
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    //設定顯示為真愛的唯一條件
     @IBAction func selectTrueLove(_ sender: Any) {
-        if zodiacTextField.text == "雙魚"
-            && ageLabel.text == "32"
-            && genderSegmented.selectedSegmentIndex == 1
-            && petSegmented.selectedSegmentIndex == 0
-            && gameplayerSegmented.selectedSegmentIndex == 0
+        if zodiacTextField.text == "雙魚" //雙魚座
+            && ageLabel.text == "32" //年紀
+            && genderSegmented.selectedSegmentIndex == 1 //性別男
+            && petSegmented.selectedSegmentIndex == 0 //喜歡狗
+            && gameplayerSegmented.selectedSegmentIndex == 0 //喜歡打電動
+            && stepperValue.text == "3" //喜歡的數字
                 {
-            answerImage.image = UIImage(named: "dove-2")
-            answerLabel.text = "YES YOU ARE!"
-        } else {
-            answerImage.image = UIImage(named: "dove-3")
-            answerLabel.text = "NO HELL NO!"
+            answerImage.image = UIImage(named: "dove-2") //符合以上條件就顯示dove-2圖
+            answerLabel.text = "YES YOU ARE!" //以及這串文字
+        } else { //如果以上不符合
+            answerImage.image = UIImage(named: "dove-3") //不符合以上條件就顯示dove-3
+            answerLabel.text = "NO HELL NO!" //以及這串文字
         }
     }
     
+    // 設置年齡Slider，可調整顯示歲數大小
     @IBAction func ageSlider(_ sender: Any) {
         ageLabel.isHidden = false
         ageLabel.text = String(Int(ageSliderControl.value))
+    }
+    
+    //設置最愛數字Stepper，設定最大最小值，並可從最小值重來
+    @IBAction func numStepper(_ sender: UIStepper) {
+        numValue = Int(sender.value)
+        stepperValue.text = "\(numValue)"
+        numberStepper.maximumValue = 9 //設定最大值
+        numberStepper.minimumValue = 0 //設定最小值
+        numberStepper.wraps = true // 到最大值以後，再按一次會從最小值繼續加
     }
     
 }
